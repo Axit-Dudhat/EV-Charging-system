ca4c899f4edbea919eb64b2d64d2beb909c60c02
Axit Dudhat <akshitdudhat5@gmail.com>
2026-06-28 21:50:59 +0530

    Update stationService.js

--- Frontend/src/services/stationService.js
+++ Frontend/src/services/stationService.js
@@ -6,7 +6,7 @@ export const createStation=(data)=>{
     return API.post("/stations",data);
 }
 export const updatedStation =(id,data)=>{
-    return API.push(`/stations/${id}`,id,data);
+    return API.put(`/stations/${id}`,data);
 }
 export const deleteStation=(id)=>{
     return API.delete(`/stations/${id}`);
