a3a46ab9b4692ad30120b413084fabaef84815ec
Axit Dudhat <akshitdudhat5@gmail.com>
2026-06-28 21:20:21 +0530

    Update axios.js

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://ev-charging-system-production.up.railway.app/",
+    baseURL: "https://ev-charging-system-production.up.railway.app/api",
 });
 
 API.interceptors.request.use((config) => {
