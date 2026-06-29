78a4d8f138722a05a1794e91272ef26a5e751927
Axit Dudhat <akshitdudhat5@gmail.com>
2026-06-29 11:25:23 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -16,7 +16,7 @@ app.use("/api/auth",authRoutes);
 app.use("/api/stations",router)
 
 app.get("/",(req,res)=>{
-    res.send("run")
+    res.send("EV Charging System API is running successfully 🚀")
 });
 const port=process.env.PORT||5000;
 app.listen(port,()=>{
